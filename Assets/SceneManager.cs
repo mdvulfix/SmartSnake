a1f0b4123bbef46cfb4bbcf1cd5ddae504acc5ed
mdvulfix <mdvulfix@users.noreply.github.com>
2019-12-03 01:03:57 +0300

    add SceneManager, SceneObject clsses
    add SceneManager, SceneObject clsses

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    
+    SceneObject map;
+    
+    
+    void Awake()
+    {
+
+
+    }
+
+    void Start()
+    {
+        
+        map = new SceneObject(new GameObject(), "Map");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SceneObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneObject : MonoBehaviour
+{
+    
+    private string name;
+    private GameObject obj;
+
+    public SceneObject(GameObject obj, string name)
+    {
+        
+        SetName(name);
+        SetObject(obj);
+
+
+    }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    public void SetObject(GameObject obj)
+    {
+        this.obj = obj;
+        this.obj.name = GetName();
+    }
+
+
+
+    public string GetName()
+    {
+        return name;
+    }
+
+
+
+}
